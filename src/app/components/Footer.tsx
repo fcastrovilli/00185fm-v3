f6555c76c2838f89f153d25ce262220ca2c19433
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2024-08-20 13:18:05 +0200

    feat: add tailwind

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default async function Footer() {
+  return (
+    <footer className="flex h-20 items-center border-t px-4">
+      <div className="text-3xl font-semibold">footer</div>
+    </footer>
+  )
+}
---
+++ src/app/components/Nav.tsx
@@ -0,0 +1,8 @@
+export default function Nav() {
+  return (
+    <nav className="flex h-20 items-center justify-between border-b px-4">
+      <div className="text-3xl font-semibold">00185fm</div>
+      <div>other links</div>
+    </nav>
+  )
+}
