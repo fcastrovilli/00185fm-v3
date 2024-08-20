f7131d1616b2ae251056ac49b5acc10924cbc46d
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2024-08-20 14:16:56 +0200

    feat: basic data handle

--- src/app/(frontend)/archive/page.tsx
+++ src/app/(frontend)/archive/page.tsx
@@ -1,7 +1,24 @@
-export default function Archive() {
+import configPromise from '@payload-config'
+import { getPayloadHMR } from '@payloadcms/next/utilities'
+
+export default async function Archive() {
+  const payload = await getPayloadHMR({ config: configPromise })
+
+  const episodes = await payload.find({
+    collection: 'episodes',
+    depth: 1,
+    limit: 12,
+  })
   return (
     <div className="p-4">
       <h1>Archive</h1>
+      <ul>
+        {episodes.docs.map((episode) => (
+          <li key={episode.slug}>
+            <a href={`/archive/${episode.slug}`}>{episode.title}</a>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -2,7 +2,9 @@ export default function Nav() {
   return (
     <nav className="flex h-20 items-center justify-between border-b px-4">
       <div className="text-3xl font-semibold">00185fm</div>
-      <div>other links</div>
+      <div>
+        <a href="/archive">Archive</a>
+      </div>
     </nav>
   )
 }
