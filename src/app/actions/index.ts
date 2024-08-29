9ba62305911561cd965de1b97b8fe13f1c284dd8
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2024-08-29 20:49:57 +0200

    feat: added `getPaginatedEpisodesByShow`, `getPaginatedEpisodesByArtist`

--- src/app/actions/index.ts
+++ src/app/actions/index.ts
@@ -22,3 +22,67 @@ export const getPaginatedEpisodes = cache(async ({ pageParam }: { pageParam?: nu
   })
   return result
 })
+
+export const getPaginatedEpisodesByShow = cache(
+  async ({ show, pageParam }: { show: string | null | undefined; pageParam?: number }) => {
+    const { isEnabled: draft } = draftMode()
+
+    const result = await payload.find({
+      collection: 'episodes',
+      limit: 12,
+      draft,
+      overrideAccess: true,
+      pagination: true,
+      page: pageParam,
+      depth: 1,
+      where: {
+        and: [
+          {
+            'show.slug': {
+              equals: show,
+            },
+          },
+          {
+            public: {
+              equals: true,
+            },
+          },
+        ],
+      },
+    })
+
+    return result
+  },
+)
+
+export const getPaginatedEpisodesByArtist = cache(
+  async ({ artistId, pageParam }: { artistId: string | null | undefined; pageParam?: number }) => {
+    const { isEnabled: draft } = draftMode()
+
+    const result = await payload?.find({
+      collection: 'episodes',
+      limit: 12,
+      depth: 1,
+      page: pageParam,
+      pagination: true,
+      draft,
+      overrideAccess: true,
+      where: {
+        and: [
+          {
+            curatedBy: {
+              equals: artistId,
+            },
+          },
+          {
+            public: {
+              equals: true,
+            },
+          },
+        ],
+      },
+    })
+
+    return result
+  },
+)
